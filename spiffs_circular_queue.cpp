9fbff07adf92375bdc5e4101b3b53962497ec78d
Vladislav Rykov <Vladislav Rykov>
2019-01-13 23:03:36 +0100

    first commit

---
+++ spiffs_circular_queue.cpp
@@ -0,0 +1,121 @@
+#include "spiffs_circular_queue.h"
+#include "sys/stat.h"
+
+const char send_queue_filename[] = "/spiffs/send_queue.data";
+
+static uint16_t _front = 0;
+static uint16_t _back = 0;
+static uint16_t _size = 0;
+static FILE     * _fd;
+
+uint8_t _mount_spiffs(void);
+void    _unmount_spiffs(void);
+
+uint8_t spiffs_circular_queue_init(uint16_t front, uint16_t back) {
+    uint8_t ret = 0;
+
+    if (_mount_spiffs()) {
+        _back = back;
+        _front = front;
+        _size = (_back >= _front? (_back - _front) : (SPIFFS_CIRCULAR_QUEUE_MAX_SIZE - _front + _back));
+
+        struct stat sb;
+
+        if (stat(send_queue_filename, &sb)) {
+            if ((_fd = fopen(send_queue_filename, "w"))) {
+                fclose(_fd);
+
+                ret = 1;
+            }
+        }
+    }
+
+    return ret;
+}
+
+uint8_t spiffs_circular_queue_front(uint8_t * elem) {
+    uint8_t ret = 0;
+
+    if (!spiffs_circular_queue_is_empty()) {
+        if ((_fd = fopen(send_queue_filename, "r+b"))) {
+            fseek(_fd, _front * SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE, SEEK_SET);
+            
+            ret = SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE == fread(elem, 1, SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE, _fd);
+            
+            fclose(_fd);
+        }
+    }
+
+    return ret;
+}
+
+uint8_t spiffs_circular_queue_enqueue(uint8_t * elem) {
+    uint8_t ret = 0;
+
+    if (spiffs_circular_queue_size() < SPIFFS_CIRCULAR_QUEUE_MAX_SIZE) {
+        if ((_fd = fopen(send_queue_filename, "r+b"))) {
+            fseek(_fd, _back * SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE, SEEK_SET);
+            
+            size_t nwritten;
+            if ((ret = (SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE == (nwritten = fwrite(elem, 1, SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE, _fd))))) {
+                _back = (++_back) % SPIFFS_CIRCULAR_QUEUE_MAX_SIZE;
+                _size++;
+            }
+            
+            fclose(_fd);        
+        }
+    }
+
+    return ret;
+}
+
+void spiffs_circular_queue_dequeue() {
+    if (!spiffs_circular_queue_is_empty()) {
+        _front = (++_front) % SPIFFS_CIRCULAR_QUEUE_MAX_SIZE;
+        _size--;
+    }
+}
+
+uint8_t spiffs_circular_queue_is_empty() {
+    return !_size && (_front == _back);
+}
+
+uint16_t spiffs_circular_queue_size() { 
+    return _size;
+}
+
+uint16_t spiffs_circular_queue_get_front_ptr() {
+    return _front;
+}
+
+uint16_t spiffs_circular_queue_get_back_ptr() {
+    return _back;
+}
+
+uint8_t spiffs_circular_queue_free(void) {
+    uint8_t ret = 0;
+
+    if (!remove(send_queue_filename)) {
+        _unmount_spiffs();
+
+        ret = 1;
+    }
+
+    return ret;
+}
+
+uint8_t _mount_spiffs(void) {
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = "/spiffs",
+        .partition_label = NULL,
+        .max_files = 2,
+        .format_if_mount_failed = true
+    };
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    
+    return (ret == ESP_OK);
+}
+
+void _unmount_spiffs(void) {
+    esp_vfs_spiffs_unregister(NULL);
+}
---
+++ spiffs_circular_queue.h
@@ -0,0 +1,76 @@
+#ifndef __SPIFFS_CIRCULAR_QUEUE_MANAGER__H__
+#define __SPIFFS_CIRCULAR_QUEUE_MANAGER__H__
+
+#define SPIFFS_CIRCULAR_QUEUE_MAX_SIZE            (256)
+#define SPIFFS_CIRCULAR_QUEUE_ITEM_SIZE           (256)
+
+#include <Arduino.h>
+
+/* 
+ *	Initializes the library and sets current front and back queue indices (useful for ESP32 MCU)
+ *	
+ *	@param front 			[IN]	Index to current front element
+ *	@param back 			[IN]	Index to current back element
+  *	@return					1 on success and 0 on fail
+ */
+uint8_t 	spiffs_circular_queue_init(uint16_t front = 0, uint16_t back = 0);
+
+/* 
+ *	Copies front element of the queue into elem pointer. 
+ *	
+ *	@param front 			[OUT]	Pointer to buffer
+ *
+ *	@return					1 on success and 0 on fail
+ */
+uint8_t spiffs_circular_queue_front(uint8_t *elem);
+
+/* 
+ *	Enqueues elem to the front of the queue. 
+ *	
+ *	@param front 			[IN]	Pointer to buffer
+ *
+ *	@return					1 on success and 0 on fail
+ */
+uint8_t spiffs_circular_queue_enqueue(uint8_t *elem);
+
+/* 
+ *	Pops out the first element of the queue. 
+ */
+void 	spiffs_circular_queue_dequeue(void);
+
+/* 
+ *	Checks whether the queue is empty or not.
+ *
+ *	@return					1 when empty and 0 if not
+ */
+uint8_t spiffs_circular_queue_is_empty(void);
+
+/* 
+ *	Returns the current queue size according to the current indices.
+ *
+ *	@return					queue size
+ */
+uint16_t spiffs_circular_queue_size(void);
+
+/* 
+ *	Gets the front index of the queue
+ *
+ *	@return					front index
+ */
+uint16_t spiffs_circular_queue_get_front_indx(void);
+
+/* 
+ *	Gets the back index of the queue
+ *
+ *	@return					back index
+ */
+uint16_t spiffs_circular_queue_get_back_indx(void);
+
+/* 
+ *	Frees resourses allocated for the queue and closes the SPIFFS.
+ *
+ *	@return					1 when empty and 0 if not
+ */
+uint8_t	spiffs_circular_queue_free(void);
+
+#endif // __SPIFFS_CIRCULAR_QUEUE_MANAGER__H__
